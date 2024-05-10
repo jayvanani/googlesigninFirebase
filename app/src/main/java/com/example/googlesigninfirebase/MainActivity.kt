a7f32e049fe9ca4c00a243ba0183d3cc1261001a
Jay <jayvanani31@gmail.com>
2024-05-10 14:14:45 +0530

    nnee

---
+++ app/src/main/java/com/example/googlesigninfirebase/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.googlesigninfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btSignIn: SignInButton
+    lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Assign variable
+        btSignIn = findViewById(R.id.bt_sign_in)
+
+        // Initialize sign in options the client-id is copied form google-services.json file
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("40242800397-fbifq30jdgt1t5ni1o87j6lf9du05cpr.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        // Initialize sign in client
+        googleSignInClient = GoogleSignIn.getClient(this@MainActivity, googleSignInOptions)
+        btSignIn.setOnClickListener { // Initialize sign in intent
+            val intent: Intent = googleSignInClient.signInIntent
+            // Start activity for result
+            startActivityForResult(intent, 100)
+        }
+
+        // Initialize firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        // Initialize firebase user
+        val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+        // Check condition
+        if (firebaseUser != null) {
+            // When user already sign in redirect to profile activity
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    ProfileActivity::class.java
+                ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Check condition
+        if (requestCode == 100) {
+            // When request code is equal to 100 initialize task
+            val signInAccountTask: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            // check condition
+            if (signInAccountTask.isSuccessful) {
+                // When google sign in successful initialize string
+                val s = "Google sign in successful"
+                // Display Toast
+                displayToast(s)
+                // Initialize sign in account
+                try {
+                    // Initialize sign in account
+                    val googleSignInAccount = signInAccountTask.getResult(ApiException::class.java)
+                    // Check condition
+                    if (googleSignInAccount != null) {
+                        // When sign in account is not equal to null initialize auth credential
+                        val authCredential: AuthCredential = GoogleAuthProvider.getCredential(
+                            googleSignInAccount.idToken, null
+                        )
+                        // Check credential
+                        firebaseAuth.signInWithCredential(authCredential)
+                            .addOnCompleteListener(this) { task ->
+                                // Check condition
+                                if (task.isSuccessful) {
+                                    // When task is successful redirect to profile activity
+                                    startActivity(
+                                        Intent(
+                                            this@MainActivity,
+                                            ProfileActivity::class.java
+                                        ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    )
+                                    // Display Toast
+                                    displayToast("Firebase authentication successful")
+                                } else {
+                                    // When task is unsuccessful display Toast
+                                    displayToast(
+                                        "Authentication Failed :" + task.exception!!.message
+                                    )
+                                }
+                            }
+                    }
+                } catch (e: ApiException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun displayToast(s: String) {
+        Toast.makeText(applicationContext, s, Toast.LENGTH_SHORT).show()
+    }
+}
